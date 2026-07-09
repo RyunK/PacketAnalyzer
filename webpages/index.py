339e0813af91c4329bad7e64f47656e55efc0b8a
RyunK, Lee <lrk222@naver.com>
2026-07-09 17:14:13 +0900

    feat: init pages

--- webpages/index.py
+++ webpages/index.py
@@ -1,1 +1,8 @@
 import streamlit as st
+
+pg = st.navigation([
+        st.Page('pages/home.py', title='🏠 Home'),
+        st.Page('pages/details.py', title='📈 세부 Dashboards'),
+        st.Page('pages/ipcountry.py', title='🗺️ IP Country'),
+    ])
+pg.run()
