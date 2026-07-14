efbcfae75add889776595adc27e694654756c05f
RyunK, Lee <lrk222@naver.com>
2026-07-14 10:24:08 +0900

    style: 블랙리스트, 화이트리스트 DB 분리

--- engine/db/dbmodule.py
+++ engine/db/dbmodule.py
@@ -1,9 +1,9 @@
 import sqlite3
-import time
 
 from .packets_repo import PacketRepo
 from .flows_repo import FlowRepo
 from .warnings_repo import WarningRepo
+from .black_white_list_repo import BlackWhiteRepo
 
 class DBModule:
     def __init__(self):
@@ -12,10 +12,12 @@ class DBModule:
         self.packet = PacketRepo(self)
         self.flow = FlowRepo(self)
         self.warnig_repo = WarningRepo(self)
+        self.black_white_repo = BlackWhiteRepo(self)
+
         self.create_table()
 
     def __getattr__(self, name):
-        for repo in (self.packet, self.flow, self.warnig_repo):
+        for repo in (self.packet, self.flow, self.warnig_repo, self.black_white_repo):
             if hasattr(repo, name):
                 return getattr(repo, name)
         raise AttributeError(name)
@@ -31,43 +33,10 @@ class DBModule:
         self.warnig_repo.create_warnings()
 
         # 블랙리스트, 화이트리스트 테이블
-        self.create_blackNwhites()
+        self.black_white_repo.create_blackNwhites()
 
         self.conn.commit()
 
-
-    def create_blackNwhites(self):
-       self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS black_list (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                timestamp INTEGER,
-                ip TEXT,
-                accepted INTEGER DEFAULT 0
-            );
-        ''') 
-       
-       self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS white_list (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                timestamp INTEGER,
-                ip TEXT,
-                accepted INTEGER DEFAULT 0
-            );
-        ''') 
-
-
-    def insert_white_list(self, ip:str, accepted:bool = False):
-        self.cursor.execute('''
-            INSERT INTO white_list (timestamp, ip, accepted)
-            VALUES (?, ?, ?)
-        ''', (time.time(), ip, 1 if accepted == True else 0))
-
-    def insert_black_list(self, ip:str, accepted:bool = False):
-        self.cursor.execute('''
-            INSERT INTO black_list (timestamp, ip, accepted)
-            VALUES (?, ?, ?)
-        ''', (time.time(), ip, 1 if accepted == True else 0))
-    
 
     def close(self):
         self.packet.flush()
