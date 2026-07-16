7ecd35054f909c6529ca88d079517dffc920aa4d
CHR9705 <changhoryu97@gmail.com>
2026-07-16 09:42:51 +0900

    update: 페이지 경로 추가

--- index.py
+++ index.py
@@ -2,8 +2,10 @@ import streamlit as st
 
 pg = st.navigation([
         st.Page('webpages/pages/home.py', title='🏠 Home'),
-        st.Page('webpages/pages/details.py', title='📈 Details'),
         st.Page('webpages/pages/ipcountry.py', title='🗺️ IP Country'),
         st.Page('webpages/pages/settings.py', title='settings'),
+        st.Page('webpages/pages/testing.py', title='test'),
+        st.Page('webpages/pages/details.py', title='details'),
+        st.Page('webpages/pages/warning_list.py', title='warnings'),
     ])
 pg.run()
