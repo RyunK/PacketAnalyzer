ab99b7efa226c9d2a31b961fbb5c3c08e3e0408c
CHR9705 <changhoryu97@gmail.com>
2026-07-16 11:31:30 +0900

    docs: 작업용 경로지정

--- index.py
+++ index.py
@@ -3,10 +3,9 @@ import streamlit as st
 pg = st.navigation([
         st.Page('webpages/pages/home.py', title='🏠 Home'),
         st.Page('webpages/pages/ipcountry.py', title='🗺️ IP Country'),
-        st.Page('webpages/pages/warning list.py', title='⚠️ warning list'),
+        st.Page('webpages/pages/warning_list.py', title='⚠️ warnings'),
         st.Page('webpages/pages/settings.py', title='settings'),
-        st.Page('webpages/pages/testing.py', title='test'),
-        st.Page('webpages/pages/details.py', title='details'),
-        st.Page('webpages/pages/warning_list.py', title='warnings'),
+        
+        st.Page('webpages/pages/details2.py', title='details'),
     ])
 pg.run()
