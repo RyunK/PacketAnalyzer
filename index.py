ceeae5a484ed02c9cdb5e2978e2c7fe9e9c5b2b3
CHR9705 <changhoryu97@gmail.com>
2026-07-16 14:22:10 +0900

    docs: 테스트완료로 인한 경로 재설정

--- index.py
+++ index.py
@@ -5,7 +5,6 @@ pg = st.navigation([
         st.Page('webpages/pages/ipcountry.py', title='🗺️ IP Country'),
         st.Page('webpages/pages/warning_list.py', title='⚠️ warnings'),
         st.Page('webpages/pages/settings.py', title='settings'),
-        
-        st.Page('webpages/pages/details2.py', title='details'),
+        st.Page('webpages/pages/details.py', title='details'),        
     ])
 pg.run()
